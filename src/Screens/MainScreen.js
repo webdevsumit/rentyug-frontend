dd027f510e37f2c29c8b11cd631a495fd88cc276
sumitdhakad2837 <sumitdhakad2232@gmail.com>
2021-05-18 19:58:36 +0530

    bg img added but animations are left

--- src/Screens/MainScreen.js
+++ src/Screens/MainScreen.js
@@ -35,8 +35,8 @@ class MainScreen extends Component{
 	componentDidMount(){
 		const user = localStorage.getItem('user223');
 
-		//localStorage.setItem('url','https://sumit2232.pythonanywhere.com/');
-		localStorage.setItem('url','http://127.0.0.1:8000/');
+		localStorage.setItem('url','https://sumit2232.pythonanywhere.com/');
+		//localStorage.setItem('url','http://127.0.0.1:8000/');
 		
 		if((user===null) || (user==='')){
 			this.setState({'screen':'SubMainScreen','login':false});
