38eaddd4da1f124124ffb6a898d1dfa5ffd2c0ca
sumitdhakad2837 <sumitdhakad2232@gmail.com>
2021-05-19 16:07:10 +0530

    animation added to intro

--- src/Components/SiteIntro.js
+++ src/Components/SiteIntro.js
@@ -7,24 +7,93 @@ function SiteIntro(props){
 	const [logo, setLogo] = useState('');
 
 	let head1 = useRef(null);
+	let head2 = useRef(null);
+	let head3 = useRef(null);
+	let subHead1 = useRef(null);
+	let subHead2 = useRef(null);
+	let customerLink = useRef(null);
+	let leftButton = useRef(null);
+	let middleButton = useRef(null);
+	let rightButton = useRef(null);
 	
 	useEffect(()=>{
-
-		window.addEventListener('scroll',handleScroll);
+		
+		window.addEventListener('scroll',e=>{
+			
+			if (window.pageYOffset>=40) {
+				head1.current.style.opacity=1;
+				head1.current.style.transform='translatey(-50%)';
+				
+			}
+			else {
+				head1.current.style.opacity=0;
+				head1.current.style.transform='translatey(0%)';
+			};
+			
+			if (window.pageYOffset>=160) {
+				head2.current.style.opacity=1;
+				head2.current.style.transform='translatey(-50%)';
+				
+			}
+			else {
+				head2.current.style.opacity=0;
+				head2.current.style.transform='translatey(0%)';
+			};
+			
+			if (window.pageYOffset>=230) {
+				subHead1.current.style.opacity=1;
+				
+			}
+			else {
+				subHead1.current.style.opacity=0;
+			};
+			
+			if (window.pageYOffset>=280) {
+				head3.current.style.opacity=1;
+				head3.current.style.transform='translatey(-50%)';
+				
+			}
+			else {
+				head3.current.style.opacity=0;
+				head3.current.style.transform='translatey(0%)';
+			};
+			
+			if (window.pageYOffset>=360) {
+				subHead2.current.style.opacity=1;
+				
+			}
+			else {
+				subHead2.current.style.opacity=0;
+			};
+			
+			if (window.pageYOffset>=400) {
+				customerLink.current.style.opacity=1;
+				
+			}
+			else {
+				customerLink.current.style.opacity=0;
+			};
+			
+			if (window.pageYOffset>=440) {
+				leftButton.current.style.opacity=1;
+				middleButton.current.style.opacity=1;
+				rightButton.current.style.opacity=1;
+				
+			}
+			else {
+				leftButton.current.style.opacity=0;
+				middleButton.current.style.opacity=0;
+				rightButton.current.style.opacity=0;
+			};
+			
+			
+		});
 		
 		const url = localStorage.getItem('url');
 		axios.get(url+'logo/').then(res=>{
 			setLogo(res.data[0].Logo);
 		});
-		
-		return window.removeEventListener('scroll',handleScroll);
-	},[])
-
-	const handleScroll=e=>{
-		if (e.pageY-e.clientY<=200) console.log('done');
-		console.log(e.clientY);
-		console.log(e.pageY);
-	}
+	},[]);
 	
 	return(
 		<div className='SiteIntro'>
@@ -34,40 +103,44 @@ function SiteIntro(props){
 			<div className='SiteIntro-content'>
 			
 				<div className='breakpoint'></div>
+				<div className='breakpoint'></div>
 				
-				<div>
+				<div className='img-div'>
 					<img className='logo' src={logo} alt='logo'/>
 				</div>
 				<h2 className='head-des'>Our First renting social media</h2>
 
 				<div className='breakpoint'></div>
-				
-				<h3 ref={head1}>Now, you can <b>make money</b> with your old things.</h3>
-				<h3>Just give them on <b>rent</b>.</h3>
-
 				<div className='breakpoint'></div>
 				
-				<h3>Get things that you need at <b>low price</b> with <b>verification</b>.</h3>
-				<h3> Take on <b>rent</b>  or <b>purchase</b>.</h3>
+				<h3 ref={head1}>Now, you can <b>make money</b> with your old things.<br/>
+					Just give them on <b>rent</b>.</h3>
+
+				<div className='breakpoint'></div>
+				<div className='breakpoint'></div>
+				
+				<h3 ref={head2}>Get things that you need at <b>low price</b> with <b>verification</b>.<br/>
+					Take on <b>rent</b>  or <b>purchase</b>.</h3>
+					
 				<br/>
 				
-				<em>You can find the verification tag on product if the details given are true. 
+				<em ref={subHead1}>You can find the verification tag on product if the details given are true. 
 				Details with no tag may or may not be true. </em>
 
 				<div className='breakpoint'></div>
 				
-				<h3>You can also make money by shipping product.</h3>
-				<em>(comming soon)</em>
+				<h3 ref={head3}>You can also <b>make money</b> by shipping product.</h3>
+				<em ref={subHead2}>(comming soon)</em>
 
 				<div className='breakpoint'></div>
 				
-				<a href='tel:+91 7999004229'>Customer care</a>
+				<a ref={customerLink} href='tel:+91 7999004229'>Customer care</a>
 				
 				<div className='breakpoint'></div>
 				
-				<Link to='/login'><button>login</button></Link>
-				<Link to='/signup'><button>signup</button></Link>
-				<Link to='/about'><button onClick={props.openAboutUs}>about us</button></Link>
+				<Link to='/login' ref={leftButton}><button>login</button></Link>
+				<Link to='/signup' ref={middleButton}><button>signup</button></Link>
+				<Link to='/about' ref={rightButton}><button onClick={props.openAboutUs}>about us</button></Link>
 			</div>
 		</div>
 	);
